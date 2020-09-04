de9dc6b8878f2571afb2929493d5c00a0c92a9bc
András Ács <acsandras@gmail.com>
2020-09-04 13:48:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 04/09/2020
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello GitLive");
+    }
+}
